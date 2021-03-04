8956b6fe9e5cddd65a2cef1eec443487cd0b7a23
Muhammad Gebby Geovany <mg.geovany@student.ub.ac.id>
2021-03-04 11:16:17 +0700

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
-const proxy = require("http-proxy-middleware");
+const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    proxy(["/api", , "/otherApi"], { target: "https://gowes-market-apollo-server.herokuapp.com/" })
-  );
-};
+    module.exports = function(app) {
+        app.use(proxy('/api/**', { target: 'https://gowes-market-apollo-server.herokuapp.com/' }));
+        app.use(proxy('/otherApi/**', { target: 'https://gowes-market-apollo-server.herokuapp.com/' }));
+    };
