01d22b3bbb5bd280cec31e4aa73ad438ad2fd980
Muhammad Gebby Geovany <mg.geovany@student.ub.ac.id>
2021-05-07 03:21:29 +0700

    cleaning code

--- src/components/ModalAddItemReview.js
+++ src/components/ModalAddItemReview.js
@@ -46,19 +46,7 @@ function ModalAddItemReview({ item }) {
             const uploadTask = storage.ref(`images/${image.name}`).put(image);
             uploadTask.on(
                 "state_changed",
-                (snapshot) => {
-                    // // Observe state change events such as progress, pause, and resume
-                    // // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                    // var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    // console.log('Upload is ' + progress + '% done');
-                    // switch (snapshot.state) {
-                    //   case firebase.storage.TaskState.PAUSED: // or 'paused'
-                    //     console.log('Upload is paused');
-                    //     break;
-                    //   case firebase.storage.TaskState.RUNNING: // or 'running'
-                    //     console.log('Upload is running');
-                    //     break;
-                },
+                (snapshot) => {},
                 (error) => {
                     console.log(error);
                 },
